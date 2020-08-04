6432037ddad6ff6b61db8b6c1bac657feb56790a
Allan C. Marques <acmlira@gmail.com>
2020-08-04 14:10:44 -0300

    initial support for windows build: Win32 and WinCE
    Changelog:
    1.  Upgrades CMake version because the visual studio generator needs it;
    2.  Move `cpproc` files to `posix` to evade Windows and Unix intersection of includes;
    3. Process.c now excludes posix headers (*ifdef*);
    4. `errno.h` is no longer included for Windows in FileChannelImpl.c;
    5. `LOCALE_SNAME` arg doesn't exist for WinCE platform.

--- TotalCrossVM/src/nm/lang/Process.c
+++ TotalCrossVM/src/nm/lang/Process.c
@@ -4,9 +4,11 @@
 // SPDX-License-Identifier: LGPL-2.1-only
 
 #include "Process.h"
+#ifdef POSIX
 #include "cpproc.h"
 #include "errno.h"
 #include <signal.h>
+#endif
 
 TC_API void jlPI_waitFor(NMParams p) {
 #if defined(linux) && !defined(darwin)
