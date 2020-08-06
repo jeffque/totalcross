56f3474a75c8fbf83271c6bd642fb4e724386952
Allan C <acmlira@gmail.com>
2020-08-06 14:37:03 -0300

    cmake: adds license headers for new files

--- TotalCrossVM/modules/FindSkia.cmake
+++ TotalCrossVM/modules/FindSkia.cmake
@@ -1,3 +1,8 @@
+# Copyright (C) 2000-2013 SuperWaba Ltda.
+# Copyright (C) 2014-2020 TotalCross Global Mobile Platform Ltda.
+#
+# SPDX-License-Identifier: LGPL-2.1-only
+
 # Skia as graphics backend.
 # Together with its dependencies.
 # This should be a custom build from source, put into /opt/build/skia
